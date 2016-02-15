e6fac0d5f89ecfd95fff22a498770a825866a3ad
Viktor Filinkov <bruteforce@sigil.tk>
2016-02-15 06:59:49 +0600

    Simple pager and navigation bind

---
+++ app/src/main/java/org/h8code/funs/funs/DaysPagerAdapter.java
@@ -0,0 +1,43 @@
+package org.h8code.funs.funs;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by belial on 15.02.16.
+ */
+public class DaysPagerAdapter extends PagerAdapter {
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext())
+                .inflate(R.layout.days_view, container, false);
+
+        TextView textView = (TextView) view.findViewById(R.id.text);
+        textView.setText("Sample text");
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return 1000;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public String getPageTitle(int position) {
+        return "sample title";
+    }
+}
--- app/src/main/java/org/h8code/funs/funs/NavigationDrawer.java
+++ app/src/main/java/org/h8code/funs/funs/NavigationDrawer.java
@@ -3,6 +3,9 @@ package org.h8code.funs.funs;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +16,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.nshmura.recyclertablayout.RecyclerTabLayout;
+
 public class NavigationDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ViewPager daysPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,15 @@ public class NavigationDrawer extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        DaysPagerAdapter daysPagerAdapter = new DaysPagerAdapter();
+        daysPager = (ViewPager) findViewById(R.id.days_pager);
+        daysPager.setAdapter(daysPagerAdapter);
+
+        RecyclerTabLayout daysView = (RecyclerTabLayout) findViewById(R.id.recycler_tab_layout);
+        daysView.setUpWithViewPager(daysPager);
+        daysView.setHasFixedSize(true);
+
     }
 
     @Override
@@ -81,7 +97,7 @@ public class NavigationDrawer extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            daysPager.setCurrentItem(666);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
